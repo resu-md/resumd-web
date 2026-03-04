ddf1e82ca9e8fe967f6e405e4756e8543b87061d
André Rocco <99764757+andrerocco@users.noreply.github.com>
2026-03-04 14:25:19 -0300

    style: balance dropdown menu styles

--- src/components/preview/Previewer.tsx
+++ src/components/preview/Previewer.tsx
@@ -105,24 +105,24 @@ function PreviewToolbar(props: { onExportPdf: () => void; onDownloadZip: () => v
 
                 <DropdownMenu placement="bottom-end" gutter={8}>
                     <DropdownMenu.Trigger class="proeminent-button text-primary flex h-8.5 items-center gap-1 rounded-full pr-2 pl-3.5">
-                        Save <FiChevronDown class="text-label-tertiary size-5.5 translate-y-px" />
+                        Save <FiChevronDown class="text-label-tertiary size-5 translate-y-px" />
                     </DropdownMenu.Trigger>
                     <DropdownMenu.Portal>
-                        <DropdownMenu.Content class="proeminent-button flex flex-col gap-[4px] rounded-[22px] p-[6px]">
+                        <DropdownMenu.Content class="proeminent-button flex flex-col rounded-[21px] p-[5px] text-sm">
                             <DropdownMenu.Item
                                 disabled
-                                class="text-label-primary hover:bg-fill-tertiary flex h-8.5 w-full cursor-pointer items-center gap-1 rounded-full pr-4 pl-2 text-sm data-disabled:cursor-not-allowed data-disabled:opacity-50"
+                                class="text-label-primary hover:bg-fill-secondary data-disabled:text-label-secondary flex h-8.5 w-full cursor-pointer items-center gap-1 rounded-full pr-4 pl-2 data-disabled:cursor-not-allowed"
                                 title="GitHub integration coming soon."
                             >
-                                <IoLogoGithub size={18} class="text-label-secondary mr-0.5 w-6" />
-                                <span>Push to GitHub</span>
+                                <IoLogoGithub size={17} class="mr-0.5 w-6" />
+                                <span class="mt-px">Push to GitHub</span>
                             </DropdownMenu.Item>
                             <DropdownMenu.Item
-                                class="text-label-primary hover:bg-fill-tertiary flex h-8.5 w-full cursor-pointer items-center gap-1 rounded-full pr-4 pl-2 text-sm"
+                                class="text-label-primary hover:bg-fill-secondary flex h-8.5 w-full cursor-pointer items-center gap-1 rounded-full pr-4 pl-2"
                                 onSelect={props.onDownloadZip}
                             >
-                                <IoFolderOpen size={16} class="text-label-secondary mr-0.5 w-6" />
-                                <span>Download sources as .zip</span>
+                                <IoFolderOpen size={15} class="mr-0.5 w-6" />
+                                <span class="mt-px">Download sources as .zip</span>
                             </DropdownMenu.Item>
                         </DropdownMenu.Content>
                     </DropdownMenu.Portal>
